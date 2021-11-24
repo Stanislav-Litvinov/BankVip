4ff0ab8bcbd7f1baa63f930ff3016f01cc326ebb
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-11-25 02:30:27 +0300

    BankVip.java and report.md added

---
+++ src/BankVip.java
@@ -0,0 +1,10 @@
+public class BankVip {
+    public static class Main {
+        public static void main(String[] args) {
+            int balance = 2_000_000_000;
+            int transfer = 500_000_000;
+            int totalSum = balance + transfer;
+            System.out.println(totalSum);
+        }
+    }
+}
